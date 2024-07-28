bba3d1ce33d454fe039541df14e67848cd276036
Mayank Ghatawal <139001774+MayankGhatawal@users.noreply.github.com>
2024-07-28 15:53:04 +0000

    main

---
+++ Array.js
@@ -0,0 +1,48 @@
+// Started Array 
+
+// var a = [54, 423, 13, 435, 12, 5325, 123]
+
+// console.log(a[5]);
+
+/* There are so many thing's to do in the Array when I want to take some important and they are 
+found with the i found the array which i know the array. */
+
+// Normal Functions
+
+function Fun(parameters){
+    // code
+
+    return 'something' + parameters
+}
+
+// Arrow Functions
+
+const Func = (parameters) => {
+    // code
+
+    return 'something' + parameters
+}
+
+// const Func = (parameters) => return 'something' + parameters
+
+// Creating an Array
+// const Arr = ['Apple','Potato','Banana','Tomato','Water-Melon','Green-Chilli','Orange','Onion'];
+
+// Arr.forEach()
+
+// Arr.forEach((Arr)=>{
+//     console.log(Arr[2] + ' :- 1');
+// });
+
+// Map Array
+
+// const Force = Arr.map((items)=>'Banana');
+
+// console.log(Force);
+
+const company = ['Bloomberg', 'Microsoft', 'Uber', 'Google', 'IBM', 'Netflix'];
+
+const d = company.pop['Bloomberg']
+
+console.log(company)
+console.log(d);
